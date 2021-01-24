85a303ae75cf97bc6d4e94f4d090622448584582
Takahide Sato <satotakahide@satoutakaeinoMacBook-Air.local>
2021-01-24 22:15:02 +0900

    [A]modelの返り値をstrに

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    # オブジェクトの文字列表現を定義するために用いるメソッド -> オブジェクトにstrやprintを適用した場合に呼び出される
+    def __str__(self):
+        return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
